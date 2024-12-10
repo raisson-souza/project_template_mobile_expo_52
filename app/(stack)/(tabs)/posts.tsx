4c91ee1ab24cbe37964987c507858b2b970cdf93
Raisson Souza <raissonrai@gmail.com>
2024-12-10 13:38:32 -0300

    Implementada captura de parâmetros em rota.

--- app/(stack)/(tabs)/posts.tsx
+++ app/(stack)/(tabs)/posts.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router"
 import { Screen } from "@/components/base/Screen"
 import Box from "@/components/base/Box"
 import TextBold from "@/components/base/TextBold"
@@ -7,6 +8,9 @@ export default function PostsScreen() {
         <Screen>
             <Box.Column>
                 <TextBold>TELA DE POSTS</TextBold>
+                <Link href={{ pathname: "/post", params: { id: "1" }}}>Post 01</Link>
+                <Link href={{ pathname: "/post", params: { id: "2" }}}>Post 02</Link>
+                <Link href={{ pathname: "/post", params: { id: "3" }}}>Post 03</Link>
             </Box.Column>
         </Screen>
     )
--- app/(stack)/post.tsx
+++ app/(stack)/post.tsx
@@ -1,12 +1,16 @@
 import { Screen } from "@/components/base/Screen"
+import { useLocalSearchParams } from "expo-router"
 import Box from "@/components/base/Box"
 import TextBold from "@/components/base/TextBold"
 
 export default function PostScreen() {
+    const { id } = useLocalSearchParams<{ id: string }>()
+
     return (
         <Screen>
             <Box.Column>
                 <TextBold>TELA DO POST</TextBold>
+                <TextBold>POST { id }</TextBold>
             </Box.Column>
         </Screen>
     )
